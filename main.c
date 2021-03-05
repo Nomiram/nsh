f1335c1d89a23aa9b27a5ba3e42183f6561a5083
Nomiram <qwerty.mironenko@gmail.com>
2021-03-06 02:30:09 +0300

    fixed memory leaks, add args support

--- main.c
+++ main.c
@@ -5,33 +5,60 @@
 #include <malloc.h> 
 #include <errno.h> 
 #include <string.h>
+char** strtoarr(char* str);
 int main()
 {
     pid_t 	pidChild;
 	char* 	line = NULL;
     size_t 	size = 0;
+	size_t	buf_size=10;
+	
     printf("> ");
 	
 	int str_len = getline(&line, &size, stdin);
 	if(str_len==-1){
 		printf("error\n");
 	}
-	char** args = malloc(sizeof(char*));
+	char** args = (char**)malloc(sizeof(char*) * buf_size);
 	if (line[str_len-1]=='\n'){
 		line[str_len-1]='\0';
 	}
-	args [0]= line;
-    pidChild = fork();
+    char* sep=" \t";
+    char*  istr = strtok (line,sep);
+	int ptr=0;
+    for (ptr=0; istr != NULL;ptr++)
+    {
+		
+	    args[ptr]=istr;
+		
+	    if(ptr>buf_size-1){
+			args=realloc(args,sizeof(char*)*buf_size*2);
+	    }
+        istr = strtok (NULL,sep);
+    }
+	args[ptr]=NULL;
+    if((pidChild = fork())==-1){
+		perror("ERROR ");
+	}
     if(pidChild == 0)//if child
     {
+		
 		if (execvp(args[0], args) == -1) {
             perror("ERROR ");
 		}
     }
     else//if parent
     {
-		
+	waitpid(pidChild, 0,0);
+	
+	free(line);
+	free(args);
     }
-    wait(0);
+
+
     return 0;
+}
+char** strtoarr(char* str)
+{
+	return NULL;
 }
